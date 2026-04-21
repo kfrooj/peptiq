635de2f5320f7671ad3d415f9f96e4d127517a1f
Keith <keithfarrugia.vm@gmail.com>
2026-04-21 15:36:22 +0100

    update new features

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // 👈 REQUIRED for Capacitor static build
-
   images: {
-    unoptimized: true, // 👈 REQUIRED for static export
     remotePatterns: [
       {
         protocol: "https",
